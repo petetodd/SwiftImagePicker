9bc316a2b71cda7e519dd542538e8120c451d861
Peter Todd <peter@petertodd.com>
2015-09-07 10:07:26 +0100

    Initial Commit

---
+++ SwiftImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftImagePicker
+//
+//  Created by Peter Todd Air on 07/09/2015.
+//  Copyright © 2015 Bright Green Star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
