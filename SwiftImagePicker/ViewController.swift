3ae642168dc3c9a0f410abb1b08a7e22e87750cb
Peter Todd <peter@petertodd.com>
2015-09-07 17:59:33 +0100

    Implemented very simple interface to select image from Library or to use camera.

--- SwiftImagePicker/ViewController.swift
+++ SwiftImagePicker/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageVIew: UIImageView!
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Capture Image
 
+    @IBAction func butLibraryAction(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func butPhotoAction(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    // Mark: - Update Image View
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageVIew.contentMode = .ScaleAspectFit
+            imageVIew.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
 }
 
